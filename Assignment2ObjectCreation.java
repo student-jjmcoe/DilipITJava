0aac848da45e01fd7a5d988e09bfc914bf0e7a91
student-jjmcoe <150603427+student-jjmcoe@users.noreply.github.com>
2024-10-26 17:29:29 +0530

    Add files via upload

---
+++ Assignment2ObjectCreation.java
@@ -0,0 +1,31 @@
+class Assignment2ObjectCreation
+{
+      public static void main(String[] args)
+      {
+           Assignment2ObjectCreation s1 = new Assignment2ObjectCreation();
+               System.out.println("s1");
+           Assignment2ObjectCreation s2 = new Assignment2ObjectCreation();
+               System.out.println("s2");
+           Assignment2ObjectCreation s3 = new Assignment2ObjectCreation();
+               System.out.println("s3");
+           Assignment2ObjectCreation s4 = new Assignment2ObjectCreation();
+               System.out.println("s4");
+           Assignment2ObjectCreation s5 = new Assignment2ObjectCreation();
+               System.out.println("s5");
+           Assignment2ObjectCreation s6 = new Assignment2ObjectCreation();
+               System.out.println("s6");
+           Assignment2ObjectCreation s7 = new Assignment2ObjectCreation();
+               System.out.println("s7");
+           Assignment2ObjectCreation s8 = new Assignment2ObjectCreation();
+               System.out.println("s8");
+           Assignment2ObjectCreation s9 = new Assignment2ObjectCreation();
+               System.out.println("s9");
+           Assignment2ObjectCreation s10 = new Assignment2ObjectCreation();
+               System.out.println("s10");
+           //Assignment2ObjectCreation s1 = new Assignment2ObjectCreation();
+           //    System.out.println("s1");
+           // error: variable s1 is already defined in method main(String[])
+           // Assignment2ObjectCreation s1 = new Assignment2ObjectCreation();
+      }
+}
+           
