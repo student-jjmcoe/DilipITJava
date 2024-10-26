115c6f8c8e8a15f69b3dde3aa51c6053d73e60c4
student-jjmcoe <150603427+student-jjmcoe@users.noreply.github.com>
2024-10-26 17:29:08 +0530

    Add files via upload

---
+++ Assignment2Biodata.java
@@ -0,0 +1,28 @@
+class Assignment2Biodata
+{
+     public static void main(String[] args)
+     {
+          //Own biodata
+          System.out.println("My biodata");
+          byte rollNo = 7;
+          short age = 21;
+          int seatNo = 8452;
+          long MobileNo = 9234567890l;
+          float SYpercent = 77.50f;
+          double pi = 3.14159265359;
+          char FirstLaterName = 'S';
+          boolean doYouLikeJava = true;
+          String name = "Shweta";
+
+          System.out.println(rollNo);
+          System.out.println(age);
+          System.out.println(seatNo);
+          System.out.println(MobileNo);
+          System.out.println(SYpercent);
+          System.out.println(pi);
+          System.out.println(FirstLaterName);
+          System.out.println(doYouLikeJava);
+          System.out.println(name);
+     }
+}
+          
