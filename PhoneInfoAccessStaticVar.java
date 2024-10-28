01ef6bc3fe432fa378363a32dca5d7bd2c250f48
Shweta Chindage <150603427+student-jjmcoe@users.noreply.github.com>
2024-10-28 17:30:23 +0530

    Add files via upload

---
+++ PhoneInfoAccessStaticVar.java
@@ -0,0 +1,14 @@
+public class PhoneInfoAccessStaticVar
+{
+     static String componyName = "Apple";
+     static int year = 2024;
+     
+     String phoneName = "iPhone 16 Pro";
+     double price = 140000.00;
+
+     public static void main(String[]args)
+     {
+           System.out.println(componyName);
+           System.out.println(year);
+     }
+}
