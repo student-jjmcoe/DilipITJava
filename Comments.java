8ce271ab0bc24feec993ecc039ee8d7aa0aaa2d4
Shweta Chindage <150603427+student-jjmcoe@users.noreply.github.com>
2024-11-09 18:09:32 +0530

    Add files via upload

---
+++ Comments.java
@@ -0,0 +1,15 @@
+/*
+ project name = Green project
+ team size = 6;
+ team lead =Shweta
+ */
+public class Comments //class declaration
+{
+	//class starts
+	public static void main(String[] args)
+	{
+		//main starts
+		System.out.println("Shweta");//printing statement
+	}//main end
+
+}//class ends 
