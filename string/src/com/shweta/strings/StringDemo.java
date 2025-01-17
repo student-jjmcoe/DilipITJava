e91904520fb6cb62fc881b169184aeea0d761f6f
Shweta Chindage <150603427+student-jjmcoe@users.noreply.github.com>
2025-01-17 19:38:05 +0530

    Add files via upload

---
+++ string/src/com/shweta/strings/StringDemo.java
@@ -0,0 +1,13 @@
+package com.shweta.strings;
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+		String name = new String("Shweta");
+        System.out.println(name);
+        System.out.println(name.hashCode());
+        System.out.println("Hello "+name);
+        System.out.println(name.charAt(1));
+	}
+
+}
